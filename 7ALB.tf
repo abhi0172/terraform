b314fbb1af4384d05eedb0c49b03273c0712a36b
abhi0172 <112243042+abhi0172@users.noreply.github.com>
2023-03-22 23:26:08 +0530

    Update 7ALB.tf

--- 7ALB.tf
+++ 7ALB.tf
@@ -1,13 +1,13 @@
 # Creating External LoadBalancer
 resource "aws_lb" "external-alb" {
-  name               = "ExternalLB"
+  name               = "External-LB"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.demosg.id]
   subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-1.id]
 }
 resource "aws_lb_target_group" "target-elb" {
-  name     = "ALB TG"
+  name     = "ALB-TG"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.vpc_1.id
