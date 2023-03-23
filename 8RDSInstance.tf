072d1f743d13f604709bcfa1187dbdbb25da9396
abhi0172 <112243042+abhi0172@users.noreply.github.com>
2023-03-23 17:33:50 +0530

    Update 8RDSInstance.tf

--- 8RDSInstance.tf
+++ 8RDSInstance.tf
@@ -1,7 +1,7 @@
 # Creating RDS Instance
 resource "aws_db_subnet_group" "default" {
   name       = "main"
-  subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-1.id]
+  subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-2.id]
 tags = {
   Name = "My DB subnet group"
 }
@@ -10,7 +10,7 @@ resource "aws_db_instance" "default" {
   allocated_storage      = 10
   db_subnet_group_name   = aws_db_subnet_group.default.id
   engine                 = "mysql"
-  engine_version         = "8.0.20"
+  engine_version         = "5.7"
   instance_class         = "db.t2.micro"
   multi_az               = true
   db_name                = "mydb"
