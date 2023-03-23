4f659cd82390e1f5c62db400bb2f787789f8cc08
abhi0172 <112243042+abhi0172@users.noreply.github.com>
2023-03-23 18:58:39 +0530

    Update 4Ec2.tf

--- 4Ec2.tf
+++ 4Ec2.tf
@@ -1,27 +1,25 @@
 # Creating 1st EC2 instance in Public Subnet
 resource "aws_instance" "demoinstance" {
-  ami                         = "ami-05afd67c4a44cc983"
+  ami                         = "ami-02f3f602d23f1659d"
   instance_type               = "t2.micro"
-  key_name                    = "Linuxkey"
+  key_name                    = "KeyVirginia"
   vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
   subnet_id                   = "${aws_subnet.public-subnet-1.id}"
   associate_public_ip_address = true
   user_data                   = "${file("data.sh")}"
-  availability_zone = "ap-south-1b"
 tags = {
   Name = "My Public Instance 1"
 }
 }
 # Creating 2nd EC2 instance in Public Subnet
 resource "aws_instance" "demoinstance1" {
-  ami                         = "ami-05afd67c4a44cc983"
+  ami                         = "ami-02f3f602d23f1659d"
   instance_type               = "t2.micro"
-  key_name                    = "Linuxkey"
+  key_name                    = "KeyVirginia"
   vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
   subnet_id                   = "${aws_subnet.public-subnet-2.id}"
   associate_public_ip_address = true
   user_data                   = "${file("data.sh")}"
-  availability_zone = "ap-south-1a"
 tags = {
   Name = "My Public Instance 2"
 }
