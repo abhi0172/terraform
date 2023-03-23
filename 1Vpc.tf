7bdd9b42c9954a10d1a6a72ed2765db0f273199f
abhi0172 <112243042+abhi0172@users.noreply.github.com>
2023-03-23 18:57:39 +0530

    Update 1Vpc.tf

--- 1Vpc.tf
+++ 1Vpc.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.59.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
 # Creating VPC
 resource "aws_vpc" "vpc_1" {
   cidr_block       = "${var.vpc_cidr}"
