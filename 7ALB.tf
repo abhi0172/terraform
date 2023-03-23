110c02938774d570d927da40ec26b87554560aca
abhi0172 <112243042+abhi0172@users.noreply.github.com>
2023-03-23 18:59:23 +0530

    Update 7ALB.tf

--- 7ALB.tf
+++ 7ALB.tf
@@ -7,21 +7,20 @@ resource "aws_lb" "external-alb" {
   subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
 }
 resource "aws_lb_target_group" "target-elb" {
-  name     = "ALB-TG"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.vpc_1.id
 }
 resource "aws_lb_target_group_attachment" "attachment" {
-  target_group_arn = aws_lb.external-alb.arn
+  target_group_arn = aws_lb_target_group.target-elb.arn
   target_id        = aws_instance.demoinstance.id
   port             = 80
 depends_on = [
   aws_instance.demoinstance,
 ]
 }
-resource "aws_lb_target_group_attachment" "attachment-1" {
-  target_group_arn = aws_lb.external-alb.arn
+resource "aws_lb_target_group_attachment" "attachment1" {
+  target_group_arn = aws_lb_target_group.target-elb.arn
   target_id        = aws_instance.demoinstance1.id
   port             = 80
 depends_on = [
@@ -34,11 +33,6 @@ resource "aws_lb_listener" "external-elb" {
   protocol          = "HTTP"
 default_action {
   type             = "forward"
-  target_group_arn = aws_lb.external-alb.arn
+  target_group_arn = aws_lb_target_group.target-elb.arn
 }
-}
-# Getting the DNS of load balancer
-output "lb_dns_name" {
-  description = "The DNS name of the load balancer"
-  value       = "${aws_lb.external-alb.dns_name}"
 }
